7dad1784d5e5827e71d105f1fb576bf2d0202670
ducmanh02 <89408872+ducmanh02@users.noreply.github.com>
2023-02-03 23:26:54 +0700

    submit

---
+++ my-app/src/components/Content/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./style.css"
+
+const Content = () => {
+  return (
+    <div className='content_main'>This is Content Area</div>
+  )
+}
+
+export default Content
---
+++ my-app/src/components/Sidebar/index.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "./style.css"
+const Sidebar = () => {
+  return (
+    <div className='Sidebar'>
+      <div className='Sidebar_item'>
+        <a href='/'>Dashboard</a>
+      </div>
+      <div className='Sidebar_item'>
+        <a href='/'>Dashboard</a>
+      </div>
+      <div className='Sidebar_item'>
+        <a href='/'>Dashboard</a>
+      </div>
+      <div className='Sidebar_item'>
+        <a href='/'>Dashboard</a>
+      </div>
+      <div className='Sidebar_item'>
+        <a href='/'>Dashboard</a>
+      </div>
+      <div className='Sidebar_item'>
+        <a href='/'>Dashboard</a>
+      </div>
+    </div>
+  )
+}
+
+export default Sidebar
